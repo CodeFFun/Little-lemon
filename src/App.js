d13576c154601823502f76a2edb2729043eea02d
CodeFFun <sharmasanketa899@gmail.com>
2023-04-20 18:52:31 +0545

    icons and pictures

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Header from "./component/Header";
-import Main from "./component/Main";
-import Footer from "./component/Footer";
+// import Main from "./component/Main";
+// import Footer from "./component/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
-      <Footer />
+      {/* <Main /> */}
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import Logo from '../icons_assets/Logo .svg'
 
 function Nav() {
   return (
-    <>
-        <img src="" alt="logo" />
-        <ul>
-            <li> <a href=""></a> </li>
-            <li> <a href=""></a> </li>
-            <li> <a href=""></a> </li>
-            <li> <a href=""></a> </li>
+   <>
+      <div className="navbar">
+       <a href="/" className='logo' >
+        <img src={Logo} alt="logo" />
+        </a>
+        <ul className='nav-content'>
+          <li><a href="/">Home</a></li>
+          <li><a href="/">About</a></li>
+          <li><a href="/">Menu</a></li>
+          <li><a href="/">Reservations</a></li>
+          <li><a href="/">Order Online</a></li>
+          <li><a href="/">Login</a></li>
         </ul>
-    </>
+      </div>
+   </>
   )
 }
 
