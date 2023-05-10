ce8845ef6a5deb9d01397137f35bd7c155764a1e
CodeFFun <sharmasanketa899@gmail.com>
2023-05-10 10:15:38 +0545

    Final commit

--- src/component/Bookings.jsx
+++ src/component/Bookings.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import {useState} from 'react'
 
 function Bookings() {
+  const [formGroup, setFormGroup] = useState({
+    name:'',
+    date: '',
+    time: '',
+    ocassion: '',
+  })
+
+  const onChange = (e) => {
+    setFormGroup({
+      ...formGroup,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(formGroup)
+  }
+
   return (
-    <div>
-      Hello hello mic testing
-    </div>
+    <>
+     <form className='form-control' onSubmit={onSubmit}>
+      <div className="input-space">
+        <input type='text' required = 'required' name='name' value={formGroup.name} onChange={onChange} placeholder='Reservation for'/>
+      </div>
+      <div className="input-space" >
+        <input type="date" required='required' name = 'date' onChange={onChange} value={formGroup.date} />
+      </div>
+      <div className="input-space">
+        <input type='time' required = 'required' name='time' onChange={onChange} value={formGroup.time}/>
+      </div>
+      <div className="input-space" >
+        <input type='text' required = 'required'  name = 'ocassion' onChange={onChange} value={formGroup.ocassion} placeholder='ocassion'/>
+      </div>
+      <input type='submit' value = 'Submit' className='btn'/>
+      </form>
+    </>
   )
 }
 
